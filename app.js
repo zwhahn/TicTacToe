4d2019b0fffbd4f7f7c56b33c218bd8c40138bcf
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-12-13 18:42:48 -0800

    Add check for winner function

--- app.js
+++ app.js
@@ -12,7 +12,32 @@ const boardOperation = (function () {
         return board;
     }    
 
-    return { updateBoard, getBoard }
+    const checkForWinner = function () {
+        // Check rows and columns
+        for (let i = 0; i < 3; i++) {
+            // Check rows
+            if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                return board[i][0]; // Return the winner ('X' or 'O')
+            }
+            // Check columns
+            if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+                return board[0][i]; // Return the winner ('X' or 'O')
+            }
+        }
+    
+        // Check diagonals
+        if (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            return board[0][0]; // Return the winner ('X' or 'O')
+        }
+        if (board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+            return board[0][2]; // Return the winner ('X' or 'O')
+        }
+    
+        // No winner found
+        return false;
+    }
+
+    return { updateBoard, getBoard, checkForWinner }
 })();
 
 function createPlayer(name, mark) {
@@ -20,13 +45,13 @@ function createPlayer(name, mark) {
 }
 
 function gamePlay() {
-
     const takeTurn = function (row, col, player) {
         // Check that play location is on board
         if (row > 2 | col > 2){
-            return 'Error'
+            return 'Outside of board'
         }
         boardOperation.updateBoard(row, col, player.mark);
+        return boardOperation.checkForWinner();
     }
 
     return { takeTurn };
@@ -44,6 +69,8 @@ console.log(createPlayer(player2));
 
 game = gamePlay();
 
-console.log(game.takeTurn(0,1, player1));
+console.log(game.takeTurn(0,0, player2));
+console.log(game.takeTurn(1,0, player2));
+console.log(game.takeTurn(2,0, player2));
 console.log(boardOperation.getBoard())
 
